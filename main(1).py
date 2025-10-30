6862fd62c47517d7f668b2dfbb4bb5fa83b4440d
JuanCalderon94 <i2321761@continental.edu.pe>
2025-10-30 10:46:25 -0500

    JUANITO

---
+++ main(1).py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pusher
+
+app = Flask(__name__)
+CORS(app)
+
+pusher_client = pusher.Pusher(
+  app_id = "2068346",
+  key = "5398827de005ac67b6b2",
+  secret = "342442a921382d60170b",
+  cluster = "us2",
+  ssl = True
+)
+
+@app.route('/', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    message = data.get('message')
+    senderId = data.get('senderId')
+    channel = data.get('channel', 'josue-juanito')
+    pusher_client.trigger(channel, 'my-event', {'message': message, 'senderId': senderId})
+    return jsonify({'status': 'success'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
